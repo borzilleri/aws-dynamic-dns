9d6accc27aa2acd684231a7e45d96ff31618407f
Jonathan Borzilleri <jonathan@borzilleri.net>
2023-07-15 17:31:35 -0700

    Cleanup logic/logging around ip comparison
    * print found ip address in the method where we find them
    * just do the comparison in the main body, no need for an additional
      function

--- main.py
+++ main.py
@@ -23,14 +23,16 @@ def _get_route53_client(aws_profile) -> Route53Client:
 
 def _get_public_ip() -> ipaddress.IPv4Address:
     try:
-        public_ip = (
+        response = (
             urllib.request.urlopen("http://checkip.amazonaws.com/")
             .read()
             .decode("utf8")
         )
-        return ipaddress.ip_address(public_ip.strip())
+        ip = ipaddress.ip_address(response.strip())
+        print(f"Found public IP Address: {str(ip)}")
+        return ip
     except Exception as e:
-        print("Error retrieving public ip:\n", e)
+        print("Error retrieving public IP Address:\n", e)
         sys.exit(1)
 
 
@@ -41,25 +43,17 @@ def _get_configured_ip(client, zone_id, hostname) -> ipaddress.IPv4Address:
         RecordType="A",
     )
     if response["RecordData"]:
-        return ipaddress.ip_address(response["RecordData"][0])
+        ip = ipaddress.ip_address(response["RecordData"][0].strip())
+        print(f"{hostname}: Found configured IP Address: {str(ip)}")
+        return ip
     else:
+        print(f"{hostname}: No configured IP Address found.")
         return None
 
 
 def _get_timestamp() -> str:
     utc_now = datetime.datetime.now(datetime.timezone.utc)
     return utc_now.strftime("%Y-%m-%d %H:%M:%S %Z%z")
-
-
-def host_needs_update(
-    client: Route53Client, zone_id: str, hostname: str, public_ip: ipaddress.IPv4Address
-) -> bool:
-    host_ip = _get_configured_ip(client, zone_id, hostname)
-    if host_ip is None:
-        print(f"{hostname}: No ip configured.")
-    else:
-        print(f"{hostname}: Configured ip is {host_ip}")
-    return host_ip != public_ip
 
 
 def update_dns_record(
@@ -94,7 +88,7 @@ def update_dns_record(
         ]
     }
     if DEBUG_LOG:
-        pprint(dns_change_batch, indent=2, compact=True)
+        pprint(dns_change_batch, indent=2, width=120)
     if not DRY_RUN:
         response = client.change_resource_record_sets(
             HostedZoneId=zone_id, ChangeBatch=dns_change_batch
@@ -113,17 +107,17 @@ if __name__ == "__main__":
     if DEBUG_LOG:
         pprint(config)
 
-    public_ip = _get_public_ip()
-    r53_client = _get_route53_client(config["aws"]["profile"])
-
     hostname = str(config["dns"]["hostname"])
     zone_id = str(config["dns"]["zone-id"])
     ttl = int(config["dns"]["ttl"])
 
-    print(f"{hostname}: checking configured ip.")
-    needs_update = host_needs_update(r53_client, zone_id, hostname, public_ip)
-    if needs_update:
-        print(f"{hostname}: configuring new ip: {public_ip}")
-        update_dns_record(r53_client, zone_id, hostname, public_ip, ttl)
+    r53_client = _get_route53_client(config["aws"]["profile"])
+
+    public_ip = _get_public_ip()
+    host_ip = _get_configured_ip(r53_client, zone_id, hostname)
+
+    if public_ip == host_ip:
+        print(f"{hostname}: Configured address matches public address, skipping update.")
     else:
-        print(f"{hostname}: configured ip has not changed, skipping.")
+        print(f"{hostname}: Configuring new IP Address: {str(public_ip)}")
+        update_dns_record(r53_client, zone_id, hostname, public_ip, ttl)
