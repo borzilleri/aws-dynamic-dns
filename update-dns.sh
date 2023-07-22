c062e8aa0ae5a16d43246faded69bcf27e1658aa
Jonathan Borzilleri <jonathan@borzilleri.net>
2023-07-21 19:03:33 -0700

    update script should sync dependencies first

--- update-dns.sh
+++ update-dns.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+export PIPENV_VENV_IN_PROJECT=1
+/opt/homebrew/bin/pipenv sync
 /opt/homebrew/bin/pipenv run python3 \
   /opt/aws-dynamic-dns/main.py \
   /opt/aws-dynamic-dns/config.toml
